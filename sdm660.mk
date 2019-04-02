3bac650609dfb07d6b2bb5c1cc368bab704baf3b
Naomi Calabretta <me@arytonex.pw>
2019-04-02 14:34:51 +0200

    sdm660-common: Add Dirac blobs (as a separate repo)
    * They are in vendor/dirac
    
    Change-Id: I9a7b49b701d3ac93ff2e04ea578872a167df412b
    Signed-off-by: SakilMondal <mondalsakil@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -25,6 +25,9 @@
 $(call inherit-product, $(LOCAL_PATH)/properties.mk)
 PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
 
+# Dirac
+$(call inherit-product, vendor/dirac/dirac.mk)
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
