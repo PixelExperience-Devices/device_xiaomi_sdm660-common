897d369350843ea23f02ca4b47c622954e8be3fe
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2020-01-21 12:25:17 +0530

    sdm660-common: Enable power-libperfmgr
    Change-Id: Ia5f8fb4a2afc7a453eb09d2092a7872fd8442bc9

--- sdm660.mk
+++ sdm660.mk
@@ -329,7 +329,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power@1.3-service.xiaomi_sdm660-libperfmgr
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
