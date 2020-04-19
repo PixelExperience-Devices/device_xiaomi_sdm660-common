6229f0bf0c6a32b0a1c6b56774ac4f85b5e8bfc2
Sultan Alsawaf <sultan@kerneltoast.com>
2020-04-18 23:20:50 -0700

    sdm660-common: Force triple frame buffers
    This yields significantly reduced jitter and doesn't have any measurable
    impact on latency.
    
    Change-Id: I4d1de21487757be3df3bd908e1a6152fd951d732

--- properties.mk
+++ properties.mk
@@ -120,16 +120,13 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
-    debug.sf.early_app_phase_offset_ns=1500000 \
-    debug.sf.early_gl_app_phase_offset_ns=15000000 \
-    debug.sf.early_gl_phase_offset_ns=3000000 \
-    debug.sf.early_phase_offset_ns=1500000 \
     debug.sf.enable_gl_backpressure=1 \
     debug.sf.enable_hwc_vds=1 \
     debug.sf.hw=1 \
     debug.sf.latch_unsignaled=1 \
     ro.opengles.version=196610 \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     ro.surface_flinger.max_virtual_display_dimension=4096 \
     ro.surface_flinger.protected_contents=true \
     ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
