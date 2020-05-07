8e5aa088a22f6b15aed19641fd7dc91bfb29f6f4
Rajshekar Eashwarappa <reashw@codeaurora.org>
2020-05-08 00:04:18 +0530

    sdm660-common: Set swiotlb to default size
    swiotlb size set to 64MB default value.
    
    Change-Id: Ib256f5d2a1642ba2e635e632e7d1486aa2dd8f13

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -30,7 +30,7 @@ TARGET_2ND_CPU_VARIANT := cortex-a73
 TARGET_2ND_CPU_VARIANT_RUNTIME := kryo
 
 # Kernel
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 sched_enable_power_aware=1 service_locator.enable=1 swiotlb=1 androidboot.configfs=true androidboot.usbcontroller=a800000.dwc3
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 sched_enable_power_aware=1 service_locator.enable=1 androidboot.configfs=true androidboot.usbcontroller=a800000.dwc3
 BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_CMDLINE += printk.devkmsg=on
 BOARD_KERNEL_CMDLINE += usbcore.autosuspend=7
