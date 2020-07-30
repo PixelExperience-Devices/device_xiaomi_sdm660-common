ac7bd87f49edc887553d15fe035f9a5fef89b0b7
Bharat Pawar <bpawar@codeaurora.org>
2020-07-30 20:18:30 +0530

    sdm660-common: vibrator: Enable LED based vibrator
    Enable LED based vibrator for sdm660 targets.
    
    Change-Id: I6970e472701c989f634419e0784cbd6d421e4ea0

--- sdm660.mk
+++ sdm660.mk
@@ -430,6 +430,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.xiaomi_sdm660.qti
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.vibrator.service
+
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
