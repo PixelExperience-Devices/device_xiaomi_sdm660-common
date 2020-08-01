bdff0530bb33afabfc8e1012011c3cba72f67e02
Rohan Hasabe <rohanhasabe8@gmail.com>
2020-08-01 18:16:06 +0530

    sdm660-common: Drop Livedisplay

--- sdm660.mk
+++ sdm660.mk
@@ -324,11 +324,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_sdm660
 
-# LiveDisplay native
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm \
-    vendor.lineage.livedisplay@2.0-service-sysfs
-
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
