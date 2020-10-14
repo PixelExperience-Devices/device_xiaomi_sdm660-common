6472acb0199162c25c86dfa0e35173e53f449401
SakilMondal <mondalsakil@gmail.com>
2020-10-14 15:19:23 +0000

    sdm660-common: PixelExperience bringup (2/2)
    Change-Id: Ia932060da1b89e6df94724cea231f75e864812e2
    Signed-off-by: SakilMondal <mondalsakil@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -28,7 +28,7 @@ PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-pe \
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
@@ -400,10 +400,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     ipacm \
     IPACM_cfg.xml
-
-# Trust
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
 
 # Vibrator
 PRODUCT_PACKAGES += \
