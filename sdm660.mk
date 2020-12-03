a60f77c09b9e61c73cd84064b0327daca590f915
Subhajeet Muhuri <subhajeet.muhuri@aosip.dev>
2020-12-03 13:09:27 +0530

    sdm660-common: Include hardware/google/interfaces and hardware/google/pixel
     * Required by power-libperfmgr
    
    Signed-off-by: Subhajeet Muhuri <subhajeet.muhuri@aosip.dev>

--- sdm660.mk
+++ sdm660.mk
@@ -13,7 +13,9 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    hardware/google/interfaces \
+    hardware/google/pixel
 
 # Permissions
 PRODUCT_COPY_FILES += \
