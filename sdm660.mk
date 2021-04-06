de0fb3f8f807ebfa60641fdbdc794822680b7c46
fakeyatogod <fakeyatokami@gmail.com>
2021-04-06 19:22:25 +0530

    sdm660-common: Build minijail service for imsrtpservice - Also build its vendor variant
    Change-Id: Idcce131bfd8e02480558d66f2dae22520f00a3dd

--- sdm660.mk
+++ sdm660.mk
@@ -283,6 +283,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
 
+PRODUCT_PACKAGES += \
+    libavservices_minijail \
+    libavservices_minijail.vendor
+
 # OMX
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
