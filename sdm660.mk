ff4b97085b4a08fe93987d1946d2b27f7d07f07c
Ratoriku <a1063021545@gmail.com>
2021-04-26 20:18:55 +0800

    sdm660-common: Drop unused perf-profile.conf
    Signed-off-by: Ratoriku <a1063021545@gmail.com>
    Change-Id: I6446d807e4b71cbd7be8af787a375d133ab4df15

--- sdm660.mk
+++ sdm660.mk
@@ -310,9 +310,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power-service.xiaomi_sdm660-libperfmgr
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
-
 # Powerhint
 ifeq ($(EAS_POWERHINT_VARIANT), sdm636)
 PRODUCT_COPY_FILES += \
