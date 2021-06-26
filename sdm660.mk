a913f9006a7971eec1643d2cef6517856c961490
Albert I <kras@raphielgang.org>
2021-06-26 15:30:31 +0800

    sdm660-common: Inherit several Android Go configurations
    Beneficial for everyone, even on non-Go devices.
    
    Signed-off-by: Albert I <kras@raphielgang.org>
    Signed-off-by: orges <me@orgesified.ml>
    Change-Id: I088fd00500ce10df5f413cb762aaed2ec00f5dfc

--- sdm660.mk
+++ sdm660.mk
@@ -386,7 +386,9 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-service
 
 # Speed profile services and wifi-service to reduce RAM and storage
-PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := everything
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
 PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
 USE_DEX2OAT_DEBUG := false
 PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
