e45545776ac36fcb98a85d838f2be6207ef8bb29
Albert I <kras@raphielgang.org>
2021-07-31 01:26:41 +0800

    sdm660-common: Optimize build specifically for target CPU
    Signed-off-by: Albert I <kras@raphielgang.org>
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>
    Signed-off-by: Reinazhard <reinazhard@gmail.com>
    Change-Id: I308da094c5d197c09989627a8f521a629abde32f

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -25,7 +25,8 @@ TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv8-a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := kryo
+TARGET_2ND_CPU_VARIANT := cortex-a73
+TARGET_2ND_CPU_VARIANT_RUNTIME := kryo
 
 # Kernel
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 sched_enable_power_aware=1 service_locator.enable=1 swiotlb=1 androidboot.configfs=true androidboot.usbcontroller=a800000.dwc3
