3b506833f87326928029e8ecf7ddc46c6a560914
Mathieu Chartier <mathieuc@google.com>
2021-08-22 16:32:42 +0800

    sdm660-common: Always preopt extracted APKs
    Change-Id: I2e4742a796b8142f14eaad5eba81eaf800d64914
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>

--- sdm660.mk
+++ sdm660.mk
@@ -387,6 +387,7 @@ PRODUCT_PACKAGES += \
 
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
 
 # Tethering
 PRODUCT_PACKAGES += \
