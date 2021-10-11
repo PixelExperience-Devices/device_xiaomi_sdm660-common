739e5e5829afb9df48e58930cd7e0fc7066ae3c7
SakilMondal <mondalsakil@gmail.com>
2021-10-11 16:11:57 +0000

    sdm660-common: Fix recovery stuck issue
    Change-Id: I2a6e0577ba6e488334859e20569d932866449ab7

--- rootdir/etc/init.qcom.early_boot.sh
+++ rootdir/etc/init.qcom.early_boot.sh
@@ -461,3 +461,8 @@ if [ -f /sys/class/kgsl/kgsl-3d0/gpu_available_frequencies ]; then
     gpu_freq=`cat /sys/class/kgsl/kgsl-3d0/gpu_available_frequencies` 2> /dev/null
     setprop vendor.gpu.available_frequencies "$gpu_freq"
 fi
+
+rm -rf /data/system/storage.xml
+touch /data/system/storage.xml
+chattr +i /data/system/storage.xml
+
