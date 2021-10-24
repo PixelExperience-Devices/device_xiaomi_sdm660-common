5777d08cc704c1448e395944371084e30094548c
SakilMondal <mondalsakil@gmail.com>
2021-10-24 06:27:51 +0000

    Revert "sdm660-common: Update clearkey plugin to drm HAL v1.4"
    This reverts commit 275a34e0f13f1d628c5c28354f72cf66bcf70103.

--- sdm660.mk
+++ sdm660.mk
@@ -196,8 +196,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl:64 \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.4.vendor \
-    android.hardware.drm@1.4-service.clearkey
+    android.hardware.drm@1.3.vendor \
+    android.hardware.drm@1.3-service.clearkey
+
 
 # FM
 ifeq ($(BOARD_HAVE_QCOM_FM),true)
