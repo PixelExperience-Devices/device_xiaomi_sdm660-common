acd36b52e67a979e17a67262f08b0b2cb1c184ef
faham1997 <nafid.faham@pm.me>
2021-10-30 21:18:02 +0800

    sdm660-common: Switch to xiaomi common libperfmgr
    Change-Id: I96fadc7beac41d03065c4ff85d2072610c159572

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -76,6 +76,9 @@ OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
 # DRM
 TARGET_ENABLE_MEDIADRM_64 := true
 
+# DT2W
+TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
+
 # FM
 ifeq ($(BOARD_HAVE_QCOM_FM),true)
 AUDIO_FEATURE_ENABLED_FM_POWER_OPT := true
--- sdm660.mk
+++ sdm660.mk
@@ -333,7 +333,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.xiaomi_sdm660-libperfmgr \
+    android.hardware.power-service.xiaomi-libperfmgr \
     android.hardware.power.stats@1.0-service.mock
 
 # Powerhint
