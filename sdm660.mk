3186c53c8de6be6ec0ed4ef620a1eb71c3a4c6c2
Josh Wu <joshwu@google.com>
2022-08-18 12:36:22 +0530

    sdm660-common: Switch to AIDL Bluetooth audio
    Change-Id: Ide9598015b07aaaad6d7feae0de21938bf0a359d

--- sdm660.mk
+++ sdm660.mk
@@ -72,7 +72,7 @@ PRODUCT_PACKAGES += \
     libvolumelistener
 
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth.audio@2.0-impl:32 \
+    android.hardware.bluetooth.audio-impl \
     audio.bluetooth.default
 
 PRODUCT_PACKAGES += \
