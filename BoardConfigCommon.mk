cebcbd204e29c0da17e421b79e6b7b3799fb693a
Sal Savage <salsavage@google.com>
2022-08-18 12:44:49 +0530

    sdm660-common: prop: Set default the Bluetooth class of device
    The class of device is a string with a list of uint8t values:
    90,2,12
    
    The meaning is as follows:
    {Service Field, Major class, Minor class}
    
    Service Field: 0x5A -> 90
        Bit 17: Networking
        Bit 19: Capturing
        Bit 20: Object Transfer
        Bit 22: Telephony
    MAJOR_CLASS: 0x02 -> 2 (Phone)
    MINOR_CLASS: 0x0C -> 12 (Smart Phone)
    
    Bug: 217452259
    Test: make -j; -- check that bluetooth.device.class_of_device is correct
    
    Signed-off-by: karthik1896 <karthik1896@outlook.com>
    Change-Id: I3bce0d0a5e801dcadc5a26361130fa6064b10e8c

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -128,6 +128,7 @@ TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
 # Properties
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
 TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
+TARGET_SYSTEM_EXT_PROP += $(COMMON_PATH)/system_ext.prop
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
 
