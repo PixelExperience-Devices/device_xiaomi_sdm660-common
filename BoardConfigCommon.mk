9571e25d7d92ed195c9896bf3f33b2d0cadc978c
HeroBuxx <me@buxxed.tk>
2023-02-26 10:48:48 +0000

    sdm660-common: Use common SDM660 Kernel
    Signed-off-by: HeroBuxx <me@buxxed.tk>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -38,9 +38,7 @@ BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
-ifeq ($(BOARD_KERNEL_COMMON),true)
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sdm660
-endif
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
     HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
